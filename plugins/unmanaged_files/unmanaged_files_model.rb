67ac637b9be5ffefcf1442ecadc830a95a41c23c
Farah Schüller <fschueller@suse.de>
2016-05-25 15:06:52 +0200

    Assign common_attributes to pass to partial

--- plugins/unmanaged_files/unmanaged_files_model.rb
+++ plugins/unmanaged_files/unmanaged_files_model.rb
@@ -59,8 +59,8 @@ class UnmanagedFilesScope < FileScope
     end
 
     comparison = [
-      self.class.new(only_self, only_self_attributes || {}),
-      self.class.new(only_other, only_other_attributes || {}),
+      self.class.new(only_self, only_self_attributes || common_attributes ),
+      self.class.new(only_other, only_other_attributes || common_attributes ),
       changed
     ].map { |e| e.empty? ? nil : e }
 
